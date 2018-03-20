2881f70e3127e584c7ca4ad71c92c6b816de0943
sruthi <sruthirajan444@gmail.com>
2018-03-20 15:34:23 +0530

    hai

--- Client.java
+++ Client.java
@@ -1,6 +1,6 @@
     import java.net.*;  
     import java.io.*;  
-    class MyClient{  
+    class Client{  
     public static void main(String args[])throws Exception{  
     Socket s=new Socket("localhost",3333);  
     DataInputStream din=new DataInputStream(s.getInputStream());  
